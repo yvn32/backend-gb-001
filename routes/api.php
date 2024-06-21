19bcba152144748d53c3fe196f621b3b15bbdf62
Yael Velásquez <yaelvelasquez@gmail.com>
2024-06-20 20:25:37 -0400

    se comenta midleware en routes\api.php

--- routes/api.php
+++ routes/api.php
@@ -9,34 +9,34 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::prefix('v1/cuentas')->group(function () {
-//     Route::get('/',[ CuentaController::class, 'get']); //get aquí es el nombre de la función en CuentaController.php
-//     Route::post('/',[ CuentaController::class, 'create']);
-//     Route::get('/{id}',[ CuentaController::class, 'getById']);
-//     Route::put('/{id}',[ CuentaController::class, 'update']);
-//     Route::delete('/{id}',[ CuentaController::class, 'delete']);
-// });
-
-// Route::prefix('v1/mensajes')->group(function () {
-//     Route::get('/',[ MensajeController::class, 'get']);
-//     Route::post('/',[ MensajeController::class, 'create']);
-//     Route::get('/{id}',[ MensajeController::class, 'getById']);
-//     Route::put('/{id}',[ MensajeController::class, 'update']);
-//     Route::delete('/{id}',[ MensajeController::class, 'delete']);
-// });
-
-Route::prefix('v1/cuentas')->middleware(\App\Http\Middleware\CorsMiddleware::class)->group(function () {
-    Route::get('/', [\App\Http\Controllers\API\CuentaController::class, 'get']);
-    Route::post('/', [\App\Http\Controllers\API\CuentaController::class, 'create']);
-    Route::get('/{id}', [\App\Http\Controllers\API\CuentaController::class, 'getById']);
-    Route::put('/{id}', [\App\Http\Controllers\API\CuentaController::class, 'update']);
-    Route::delete('/{id}', [\App\Http\Controllers\API\CuentaController::class, 'delete']);
+Route::prefix('v1/cuentas')->group(function () {
+    Route::get('/',[ CuentaController::class, 'get']); //get aquí es el nombre de la función en CuentaController.php
+    Route::post('/',[ CuentaController::class, 'create']);
+    Route::get('/{id}',[ CuentaController::class, 'getById']);
+    Route::put('/{id}',[ CuentaController::class, 'update']);
+    Route::delete('/{id}',[ CuentaController::class, 'delete']);
 });
 
-Route::prefix('v1/mensajes')->middleware(\App\Http\Middleware\CorsMiddleware::class)->group(function () {
-    Route::get('/', [\App\Http\Controllers\API\MensajeController::class, 'get']);
-    Route::post('/', [\App\Http\Controllers\API\MensajeController::class, 'create']);
-    Route::get('/{id}', [\App\Http\Controllers\API\MensajeController::class, 'getById']);
-    Route::put('/{id}', [\App\Http\Controllers\API\MensajeController::class, 'update']);
-    Route::delete('/{id}', [\App\Http\Controllers\API\MensajeController::class, 'delete']);
+Route::prefix('v1/mensajes')->group(function () {
+    Route::get('/',[ MensajeController::class, 'get']);
+    Route::post('/',[ MensajeController::class, 'create']);
+    Route::get('/{id}',[ MensajeController::class, 'getById']);
+    Route::put('/{id}',[ MensajeController::class, 'update']);
+    Route::delete('/{id}',[ MensajeController::class, 'delete']);
 });
+
+// Route::prefix('v1/cuentas')->middleware(\App\Http\Middleware\CorsMiddleware::class)->group(function () {
+//     Route::get('/', [\App\Http\Controllers\API\CuentaController::class, 'get']);
+//     Route::post('/', [\App\Http\Controllers\API\CuentaController::class, 'create']);
+//     Route::get('/{id}', [\App\Http\Controllers\API\CuentaController::class, 'getById']);
+//     Route::put('/{id}', [\App\Http\Controllers\API\CuentaController::class, 'update']);
+//     Route::delete('/{id}', [\App\Http\Controllers\API\CuentaController::class, 'delete']);
+// });
+
+// Route::prefix('v1/mensajes')->middleware(\App\Http\Middleware\CorsMiddleware::class)->group(function () {
+//     Route::get('/', [\App\Http\Controllers\API\MensajeController::class, 'get']);
+//     Route::post('/', [\App\Http\Controllers\API\MensajeController::class, 'create']);
+//     Route::get('/{id}', [\App\Http\Controllers\API\MensajeController::class, 'getById']);
+//     Route::put('/{id}', [\App\Http\Controllers\API\MensajeController::class, 'update']);
+//     Route::delete('/{id}', [\App\Http\Controllers\API\MensajeController::class, 'delete']);
+// });
